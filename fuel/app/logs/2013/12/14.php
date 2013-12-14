83a3b0bbffb8351a3bc725b87456beab5836e5f8
Yuka Ezura <yuka.ezura@gmail.com>
2013-12-14 21:48:01 +0900

    added meeting room

--- fuel/app/logs/2013/12/14.php
+++ fuel/app/logs/2013/12/14.php
@@ -3,3 +3,12 @@
 WARNING - 2013-12-14 12:27:12 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 WARNING - 2013-12-14 12:31:08 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 WARNING - 2013-12-14 12:31:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 13:02:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 13:03:30 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 13:03:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 13:04:01 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 13:04:43 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 13:07:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 13:07:53 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 13:08:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 13:09:21 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
---
+++ fuel/app/views/room/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>online meeting</title>
+	<?php echo Asset::css('bootstrap.css'); ?>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+	<script src="https://skyway.io/dist/0.3/peer.js"></script>
+</head>
+<body>
+  <article>
+    <header>
+      <h1>チーム-- ミーティングルーム</h1>
+      <h2 id="pid"></h2>
+    </header>
+    <section>
+      <div></div>
+      <div id="chat">
+        
+      </div>
+      <div>
+        
+      </div>
+    </section>
+  </article>
+	
+</body>
+</html>
