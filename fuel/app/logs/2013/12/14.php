042dc2b066346fbb5da61ae65583a78c3094e7be
Yuka Ezura <yuka.ezura@gmail.com>
2013-12-14 20:40:25 +0900

    init

---
+++ fuel/app/logs/2013/12/14.php
@@ -0,0 +1,5 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2013-12-14 12:27:12 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 12:31:08 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-14 12:31:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
---
+++ fuel/app/views/top/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>online meeting</title>
+	<?php echo Asset::css('bootstrap.css'); ?>
+</head>
+<body>
+	
+</body>
+</html>
