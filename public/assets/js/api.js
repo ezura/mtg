e513b24aa99152a09c8f5a346279c4d3d0461c6c
Yuka Ezura <yuka.ezura@gmail.com>
2013-12-25 22:51:38 +0900

    modified api key for meeting-ez

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -1,3 +1,3 @@
-var key_ = '6165842a-5c0d-11e3-b514-75d3313b9d05';  // ローカル
+var key_ = //'6165842a-5c0d-11e3-b514-75d3313b9d05';  // ローカル
            //'4304447a-5d8a-11e3-aced-fba866690e9f';, // ezura.azurewebsites.net
-           //'cb1f407a-65f6-11e3-b1a3-8df577be8f85';  // meeting-ez.azurewebsites.net
+           'cb1f407a-65f6-11e3-b1a3-8df577be8f85';  // meeting-ez.azurewebsites.net
